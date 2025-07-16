b47babb28bffc197d94671ca10eb77ed4ec7c111
ayaanparashift <ayaan.s@parashifttech.com>
2025-07-16 17:08:33 +0530

    added caching to next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "www.kshinfra.com",
+//         pathname: "/wp-content/uploads/**",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -8,6 +23,38 @@ const nextConfig = {
         pathname: "/wp-content/uploads/**",
       },
     ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/_next/static/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable", // Cache static assets for 1 year
+          },
+        ],
+      },
+      {
+        source: "/fonts/(.*)", // if you store local fonts
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/images/(.*)", // if you store images in /public/images/
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
   },
 };
 
