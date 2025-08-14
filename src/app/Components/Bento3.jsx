46ff4bc9a4feea4b2a0bac65970b7e0b7867aa57
ayaanparashift <ayaan.s@parashifttech.com>
2025-08-14 15:40:23 +0530

    new press release added to bento slider

--- src/app/Components/Bento3.jsx
+++ src/app/Components/Bento3.jsx
@@ -4,35 +4,35 @@ import Link from "next/link";
 
 const pressReleases = [
   {
-    id: 6,
-    title:
-      "KSH INFRA forays into South India plans to invest Rs 450 Cr. on Hosur logistics park",
-    content:
-      "KSH INFRA forays into South India, plans to invest Rs 450 Cr. on Hosur logistics park",
-    image: "/press/press1.png",
-    href: "https://economictimes.indiatimes.com/industry/services/property-/-cstruction/ksh-infra-forays-into-south-india-plans-to-invest-rs-450-Cr.-on-hosur-logistics-park/articleshow/112503331.cms?from=mdr",
+    id: 1,
+    title: "KSH Infra to Invest Rs 5.5 Billion in Tamil Nadu Park.",
+    content: "KSH Infra to Invest Rs 5.5 Billion in Tamil Nadu Park.",
+    image: "/press/press6.webp",
+    href: "https://www.constructionworld.in/urban-infrastructure/warehouse-and-logistics/ksh-infra-to-invest-rs-5.5-billion-in-tamil-nadu-park/76359",
     newTab: true,
-    date: "13 Aug 2024",
+    date: "17 Jul 2025",
   },
   {
-    id: 7,
+    id: 2,
     title:
-      "KSH INFRA to invest Rs 450 crore to develop logistics park in Hosur",
+      "KSH Infra to invest ₹550 crore in second industrial park in Tamil Nadu.",
     content:
-      "KSH INFRA to invest Rs 450 crore to develop industrial & logistics park in Hosur",
-    image: "/press/press3.png",
-    href: "https://realty.economictimes.indiatimes.com/news/allied-industries/ksh-infra-to-invest-rs-450-crore-to-develop-industrial-logistics-park-in-hosur/112523130",
+      "KSH Infra to invest ₹550 crore in second industrial park in Tamil Nadu.",
+    image: "/press/press4.png",
+    href: "https://manufacturing.economictimes.indiatimes.com/news/industry/ksh-infra-to-invest-550-crore-in-second-industrial-park-in-tamil-nadu/122437140",
     newTab: true,
-    date: "14 Aug 2024",
+    date: "14 Jul 2025",
   },
   {
-    id: 8,
-    title: "KSH INFRA To Invest Nearly $54 Mn In South India Logistics Park",
-    content: "KSH INFRA To Invest Nearly $54 Mn In South India Logistics Park",
-    image: "/press/press2.png",
-    href: "https://www.vccircle.com/kshinfra-to-invest-nearly-54-mn-in-south-india-logistics-park",
+    id: 3,
+    title:
+      "KSH INFRA to invest ₹550 cr in second industrial & logistics park in Sriperumbudur.",
+    content:
+      "KSH INFRA to invest ₹550 cr in second industrial & logistics park in Sriperumbudur.",
+    image: "/press/press5.webp",
+    href: "https://www.thehindubusinessline.com/companies/ksh-infra-to-invest-550-cr-in-second-industrial-logistics-park-in-sriperumbudur/article69807276.ece",
     newTab: true,
-    date: "14 Aug 2024",
+    date: "13 Jul 2025",
   },
 ];
 
