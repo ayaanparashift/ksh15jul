4cf2602548aab162a62358ad966db4562183181b
ayaanparashift <ayaan.s@parashifttech.com>
2025-09-10 15:12:59 +0530

    SPA page views

--- src/app/Analytics.jsx
+++ src/app/Analytics.jsx
@@ -1,10 +1,22 @@
 "use client";
 import Script from "next/script";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Analytics() {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag("config", "G-EJMNK3JR62", { page_path: pathname });
+    }
+    if (window.dataLayer) {
+      window.dataLayer.push({ event: "pageview", page: pathname });
+    }
+  }, [pathname]);
+
   return (
     <>
-      {/* Google Analytics 4 (gtag.js) */}
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-EJMNK3JR62"
         strategy="afterInteractive"
