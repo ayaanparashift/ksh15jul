a6a3cba85ba89bb39271b0ea167f587520b807d1
ayaanparashift <ayaan.s@parashifttech.com>
2025-09-10 11:42:22 +0530

    analytics changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -255,14 +255,18 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         {/* GTM Script */}
-        <Script id="gtm-script" strategy="afterInteractive">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-EJMNK3JR62"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-script" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NBGQVCX');
-          `}
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-EJMNK3JR62');
+        `}
         </Script>
 
         {/* ✅ Server-side JSON-LD */}
