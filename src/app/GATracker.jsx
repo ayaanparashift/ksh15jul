7588b163e68e37a49195163f6b04d68304847eac
ayaanparashift <ayaan.s@parashifttech.com>
2025-11-14 18:03:22 +0530

    gatracker changed

--- src/app/GATracker.jsx
+++ src/app/GATracker.jsx
@@ -1,25 +1,22 @@
 "use client";
 
 import { useEffect } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export default function GATracker() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
     if (!pathname) return;
 
-    const url = pathname + searchParams.toString();
-
     if (typeof window !== "undefined" && window.gtag) {
       window.gtag("event", "page_view", {
-        page_path: url,
+        page_path: pathname,
         page_location: window.location.href,
         page_title: document.title,
       });
     }
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   return null;
 }
