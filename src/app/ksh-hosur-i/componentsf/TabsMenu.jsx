a9b6052d3cd1864de0143c388a0647fc2f51b870
ayaanparashift <ayaan.s@parashifttech.com>
2026-03-06 12:19:55 +0530

    removed brochure from hosur

--- src/app/ksh-hosur-i/componentsf/TabsMenu.jsx
+++ src/app/ksh-hosur-i/componentsf/TabsMenu.jsx
@@ -5,7 +5,7 @@ const tabs = [
   { title: "Park Highlights", link: "#park-highlight" },
   { title: "Location & Connectivity", link: "#location-connectivity" },
   { title: "Park Infrastructure", link: "#park-infrastructure" },
-  { title: "Download Brochure", link: "/brochure/KSH_Hosur_Brochure_o1_v3.pdf" },
+  // { title: "Download Brochure", link: "/brochure/KSH_Hosur_Brochure_o1_v3.pdf" },
 ];
 const TabMenu = () => <ParkTabsMenu tabs={tabs} />;
 export default TabMenu;
